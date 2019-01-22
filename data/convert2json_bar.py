26233b5f4545b70e266b8cce261a558dcb31e478
Amber <ambernobel93@gmail.com>
2019-01-22 19:02:01 +0100

    changed the json so it could be used in JS

--- data/convert2json_bar.py
+++ data/convert2json_bar.py
@@ -2,127 +2,92 @@
 # Student number: 11819359
 
 import pandas as pd
-import json
-import csv
-import numpy as np
 
-# # reads csv and converts it to a dataframe
-# df = pd.read_csv('vacc_nl.csv', encoding='cp1252')
-#
-# df['DKTP'] = df['DKTP'].str.replace(',', '.')
-# df['DKTP'] = df['DKTP'].str.strip('b')
-# df['DKTP'] = pd.to_numeric(df['DKTP'])
-#
-# df = df[['Cohort', 'DKTP', "Pneu"]]
-#
-# df.to_json('data_nl.json', orient='records')
+countries_in_eu = [
+    'Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium',
+    'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Czechia',
+    'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece',
+    'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 'Liechtenstein',
+    'Lithuania', 'Luxembourg', 'The former Yugoslav republic of Macedonia',
+    'Malta', 'Republic of Moldova', 'Monaco', 'Netherlands', 'Norway',
+    'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino',
+    'Serbia and Montenegro', 'Serbia', 'Slovakia', 'Slovenia', 'Spain',
+    'Sweden', 'Switzerland', 'Ukraine',
+    'United Kingdom of Great Britain and Northern Ireland'
+]
 
+codes = {
+    "Albania": "AL",
+    "Andorra": "AD",
+    "Austria": "AT",
+    "Azerbaijan": "AZ",
+    "Belarus": "BY",
+    "Belgium": "BE",
+    "Bosnia and Herzegovina": "BA",
+    "Bulgaria": "BG",
+    "Croatia": "HR",
+    "Cyprus": "CY",
+    "Czechia": "CZ",
+    "Denmark": "DK",
+    "Estonia": "EE",
+    "Finland": "FI",
+    "France": "FR",
+    "Georgia": "GE",
+    "Germany": "DE",
+    "Greece": "GR",
+    "Hungary": "HU",
+    "Iceland": "IS",
+    "Ireland": "IE",
+    "Italy": "IT",
+    "Kazakhstan": "KZ",
+    "Kosovo": "XK",
+    "Latvia": "LV",
+    "Liechtenstein": "LI",
+    "Lithuania": "LT",
+    "Luxembourg": "LU",
+    "The former Yugoslav republic of Macedonia": "MK",
+    "Malta": "MT",
+    "Republic of Moldova": "MD",
+    "Monaco": "MC",
+    "Montenegro": "ME",
+    "Netherlands": "NL",
+    "Norway": "NO",
+    "Poland": "PL",
+    "Portugal": "PT",
+    "Romania": "RO",
+    "Russia": "RU",
+    "San Marino": "SM",
+    "Serbia": "RS",
+    "Slovakia": "SK",
+    "Slovenia": "SI",
+    "Spain": "ES",
+    "Sweden": "SE",
+    "Switzerland": "CH",
+    "Turkey": "TR",
+    "Ukraine": "UA",
+    "United Kingdom of Great Britain and Northern Ireland": "GB",
+    "Vatican City": "VA"
+}
 
-array = ['Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium',
-            'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Czechia',
-            'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece',
-            'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 'Liechtenstein',
-            'Lithuania', 'Luxembourg', 'The former Yugoslav republic of Macedonia',
-            'Malta', 'Republic of Moldova', 'Monaco', 'Netherlands', 'Norway',
-            'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino',
-            'Serbia and Montenegro', 'Serbia', 'Slovakia', 'Slovenia', 'Spain',
-            'Sweden', 'Switzerland', 'Ukraine',
-            'United Kingdom of Great Britain and Northern Ireland']
+vacc_types = [
+    "Hib",
+    # "Men C",
+    "Pneu",
+    "DTP",
+    "Hepb",
+]
 
-codes = {"Albania": "AL",
-"Andorra": "AD",
-"Austria": "AT",
-"Azerbaijan": "AZ",
-"Belarus": "BY",
-"Belgium": "BE",
-"Bosnia and Herzegovina": "BA",
-"Bulgaria": "BG",
-"Croatia": "HR",
-"Cyprus": "CY",
-"Czechia": "CZ",
-"Denmark": "DK",
-"Estonia": "EE",
-"Finland": "FI",
-"France": "FR",
-"Georgia": "GE",
-"Germany": "DE",
-"Greece": "GR",
-"Hungary": "HU",
-"Iceland": "IS",
-"Ireland": "IE",
-"Italy": "IT",
-"Kazakhstan": "KZ",
-"Kosovo": "XK",
-"Latvia": "LV",
-"Liechtenstein": "LI",
-"Lithuania": "LT",
-"Luxembourg": "LU",
-"The former Yugoslav republic of Macedonia": "MK",
-"Malta": "MT",
-"Republic of Moldova": "MD",
-"Monaco": "MC",
-"Montenegro": "ME",
-"Netherlands": "NL",
-"Norway": "NO",
-"Poland": "PL",
-"Portugal": "PT",
-"Romania": "RO",
-"Russia": "RU",
-"San Marino": "SM",
-"Serbia": "RS",
-"Slovakia": "SK",
-"Slovenia": "SI",
-"Spain": "ES",
-"Sweden": "SE",
-"Switzerland": "CH",
-"Turkey": "TR",
-"Ukraine": "UA",
-"United Kingdom of Great Britain and Northern Ireland": "GB",
-"Vatican City": "VA"}
+for vacc_type in vacc_types:
+    csvFilePath = "vacc_eu_{}.csv".format(vacc_type)
 
-csvFilePath = "vacc_eu_Hib.csv"
+    df = pd.read_csv(csvFilePath, header=1)
 
-df = pd.read_csv(csvFilePath, header=1)
+    df = df[df['Country'].isin(countries_in_eu)]
 
-df = df[df['Country'].isin(array)]
-df = df[df['Country'].notnull()]
+    df["Country"] = df['Country'].map(codes).fillna(df['Country'])
 
-df["Country"] = df['Country'].map(codes).fillna(df['Country'])
-print(df["Country"])
+    df = df.set_index('Country')
 
-df = df.reset_index(drop=True)
-print(pd.to_numeric(df.columns, errors='ignore'))
-# df = df.replace(np.nan, "NULL")
-#
-print(df)
-#
-data = []
-for i in range(37):
-    for j in range(20):
-        data.append({
-          "year": df.columns.values[j],
-          "rate": df.iloc[i][j],
-          "type": "Hib"
-        })
-        data.append({
-          "year": df.columns.values[j],
-          "rate": df.iloc[i][j],
-          "type": "Hib"
-        })
+    df = df.apply(pd.to_numeric, errors='ignore')
 
-# print(data)
-# with open(csvFilePath) as f:
-#     reader = csv.reader(f)
-#     next(reader)
-#     # csvReader = csv.DictReader(csvFile)
-#     for row in reader:
-#         print(row)
-#         # country = csv("Country")
-#         # data[country] = row
-#         # print(row['Country'])
-# #         rows.append(row)
-
-# print(bars)
-
-# with open("vacc_bar.json", "w") as jsonfile:
-#     (json.dump(data, jsonfile))
+    df.to_json('vacc_bar_{}.json'.format(vacc_type), orient='index')

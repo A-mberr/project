ae2d91ab2eb9134e3abaefecb4a65693cc07eb37
Amber <ambernobel93@gmail.com>
2019-01-26 15:59:26 +0100

    code still buggy but works works with html and draws bar chart

--- scripts/linechart.js
+++ scripts/linechart.js
@@ -3,28 +3,30 @@
 
 (function() {
 
-function xLabels(g, g_line, xScale, height, margin) {
+function xLabels(g_line, graph) {
 
   const xAxis = d3.axisBottom()
-    .scale(xScale)
-    .tickFormat(d3.format("y")).ticks(20)
+    .scale(graph.xScale)
+    .tickFormat(d3.format('y')).ticks(20)
 
-  var gX_line = g_line.append("g")
-    .attr("transform", "translate(0," + (height - margin.top) + ")")
+  var gX_line = g_line.append('g')
+    .attr('transform', 'translate(0,' + (graph.height - graph.margin.top) + ')')
     .call(xAxis)
 }
 
-function yLabels(g, g_line, yScale, margin) {
+function yLabels(g_line, graph) {
   const yAxis = d3.axisLeft()
-    .scale(yScale)
-    .tickFormat(d => d + "%");
+    .scale(graph.yScale)
+    .tickFormat(d => d + '%');
 
-  gY_line = g_line.append("g")
-    .attr("transform", "translate(" + margin.top + ",0)")
+  gY_line = g_line.append('g')
+    .attr('transform', 'translate(' + graph.margin.top + ',0)')
     .call(yAxis);
 }
 
-function drawline(g_line, xScale, yScale, data, vaccTypes) {
+function drawline(g_line, graph, data) {
+
+  let vaccTypes = ['DTP', 'Pneu', 'Hib', 'Hepb'];
 
   const dots = [];
 
@@ -40,53 +42,54 @@ function drawline(g_line, xScale, yScale, data, vaccTypes) {
     }
   }
 
-  console.log("Dots", dots)
+  console.log('Dots', dots)
 
   const colors = {
-    "DTP": "#a6cee3",
-    "Hib": '#1f78b4',
-    "Hep b": '#b2df8a',
-    "Pneu": "#33a02c",
+    'DTP': '#a6cee3',
+    'Hib': '#1f78b4',
+    'Hep b': '#b2df8a',
+    'Pneu': '#33a02c',
   };
 
-  g_line.selectAll("circle")
+  g_line.selectAll('circle')
     .remove()
     .exit()
     .data(dots)
     .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d.year))
-    .attr("cy", d => yScale(d.rate))
-    .attr("r", 5)
-    .attr("fill", "#ffffff")
+    .append('circle')
+    .attr('cx', d => graph.xScale(d.year))
+    .attr('cy', d => graph.yScale(d.rate))
+    .attr('r', 5)
+    .attr('fill', '#ffffff')
 
     // Transition to black after some milliseconds.
     .transition()
     .duration(1000)
-    .attr("fill", d => colors[d.type]);
+    .attr('fill', d => colors[d.type]);
 
   g_line.selectAll('path')
     .remove();
 
   for (vaccType of vaccTypes) {
     const line = d3.line()
-      .x(d => xScale(d.year))
-      .y(d => yScale(d.rate));
+      .x(d => graph.xScale(d.year))
+      .y(d => graph.yScale(d.rate));
 
     g_line.append('path')
-      .attr("class", "lines " + vaccType)
-      .attr("d", line(data[vaccType]));
+      .attr('class', 'lines ' + vaccType)
+      .attr('d', line(data[vaccType]));
   }
 }
 
-function makeTitles(svg, svg_map, svg_line, width, margin) {
-  svg_line.append("text")
-    .attr("x", (width + margin.left + margin.right) / 2)
-    .attr("y", margin.top + margin.padding / 2)
-    .attr("text-anchor", "middle")
-    .text("Line chart van geselecteerde landnaam")
+function makeTitles(svg_line, graph) {
+  svg_line.append('text')
+    .attr('x', (graph.width + graph.margin.left + graph.margin.right) / 2)
+    .attr('y', graph.margin.top + graph.margin.padding / 2)
+    .attr('text-anchor', 'middle')
+    .text('Line chart van geselecteerde landnaam')
 }
-let lineVaccTypes = ['DTP', "Pneu", 'Hib', 'Hepb'];
+// TODO: verander de variabele namen naar kortere namen
+
 
 function update() {
   lineVaccTypes = [];
@@ -94,4 +97,49 @@ function update() {
     if (this.checked) lineVaccTypes.push(this.value);
   });
 }
+
+async function main() {
+
+  let lineVaccTypes = ['DTP', 'Pneu', 'Hib', 'Hepb'];
+
+  const data = {
+    'DTP' : [
+      {year:2014, rate:93},
+      {year:2015, rate:91},
+    ],
+    'Pneu' : [
+      {year:1997, rate:3},
+      {year:1998, rate:6},
+    ],
+    'Hib' : [
+      {year:2007, rate:20},
+      {year:2008, rate:28},
+    ],
+    'Hepb' : [
+      {year:2000, rate:60},
+      {year:2001, rate:61},
+    ],
+  };
+
+  xLabels(g_line, graph)
+
+  yLabels(g_line, graph)
+  drawline(g_line, graph, data)
+
+  makeTitles(svg_line, graph)
+
+}
+
+// SVG for line graph
+const svg_line = d3.select('.line')
+  .attr('width', graph.width + graph.margin.left + graph.margin.right)
+  .attr('height', graph.height + graph.margin.top + graph.margin.bottom + graph.margin.padding)
+  .style('background', 'white')
+
+const g_line = svg_line.append('g')
+  .attr('transform', 'translate(' + graph.margin.left + ','
+    + (graph.margin.top + graph.margin.padding) + ')');
+
+
+main()
 })();

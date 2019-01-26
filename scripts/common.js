d9b2c3423699591d4fe30ee21a2ecc4e340c2803
Amber <ambernobel93@gmail.com>
2019-01-26 12:46:13 +0100

    contains global variables used in other js files

---
+++ scripts/common.js
@@ -0,0 +1,31 @@
+// Name: Amber Nobel
+// Student ID: 11819359
+
+const graph = (function() {
+  const margin = {
+      top: 10,
+      right: 30,
+      bottom: 30,
+      left: 40,
+      padding: 80
+    },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+
+  const xScale = d3.scaleLinear()
+    .domain([1997, 2017])
+    .range([margin.top, width - margin.top - margin.padding]);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - margin.top, margin.top]);
+
+  return {
+    margin: margin,
+    width: width,
+    height: height,
+    xScale: xScale,
+    yScale: yScale,
+  };
+})();

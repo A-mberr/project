216e9723e1476de542c96d1d5bd6a9fecfcbc722
Amber <ambernobel93@gmail.com>
2019-01-28 14:42:49 +0100

    draw lines based on fetched line data

--- scripts/linechart.js
+++ scripts/linechart.js
@@ -1,20 +1,22 @@
 // Name: Amber Nobel
 // Student ID: 11819359
-
 (function() {
 
-function xLabels(g_line, graph) {
+function loadLineData() {
+  return d3.json('data/vacc_line.json');
+}
 
+function xLabel(g_line, graph) {
   const xAxis = d3.axisBottom()
     .scale(graph.xScale)
     .tickFormat(d3.format('y')).ticks(20)
 
-  var gX_line = g_line.append('g')
+  const gX_line = g_line.append('g')
     .attr('transform', 'translate(0,' + (graph.height - graph.margin.top) + ')')
     .call(xAxis)
 }
 
-function yLabels(g_line, graph) {
+function yLabel(g_line, graph) {
   const yAxis = d3.axisLeft()
     .scale(graph.yScale)
     .tickFormat(d => d + '%');
@@ -24,32 +26,21 @@ function yLabels(g_line, graph) {
     .call(yAxis);
 }
 
-function drawline(g_line, graph, data) {
-
-  let vaccTypes = ['DTP', 'Pneu', 'Hib', 'Hepb'];
-
+function drawLines(g_line, graph, data) {
   const dots = [];
 
   for (vaccType of vaccTypes) {
     const values =  data[vaccType];
-    for (value of values) {
+    for (const value of values) {
       dots.push({
         year: value.year,
         rate: value.rate,
         type: vaccType,
       });
-
     }
   }
 
-  console.log('Dots', dots)
-
-  const colors = {
-    'DTP': '#a6cee3',
-    'Hib': '#1f78b4',
-    'Hep b': '#b2df8a',
-    'Pneu': '#33a02c',
-  };
+  console.log('hoi')
 
   g_line.selectAll('circle')
     .remove()
@@ -60,77 +51,56 @@ function drawline(g_line, graph, data) {
     .attr('cx', d => graph.xScale(d.year))
     .attr('cy', d => graph.yScale(d.rate))
     .attr('r', 5)
-    .attr('fill', '#ffffff')
-
-    // Transition to black after some milliseconds.
-    .transition()
-    .duration(1000)
     .attr('fill', d => colors[d.type]);
 
   g_line.selectAll('path')
     .remove();
 
   for (vaccType of vaccTypes) {
-    const line = d3.line()
+    let line = d3.line()
       .x(d => graph.xScale(d.year))
       .y(d => graph.yScale(d.rate));
 
     g_line.append('path')
       .attr('class', 'lines ' + vaccType)
+      .attr('stroke', colors[vaccType])
       .attr('d', line(data[vaccType]));
   }
 }
 
-function makeTitles(svg_line, graph) {
+function makeTitle(svg_line, graph) {
   svg_line.append('text')
     .attr('x', (graph.width + graph.margin.left + graph.margin.right) / 2)
     .attr('y', graph.margin.top + graph.margin.padding / 2)
     .attr('text-anchor', 'middle')
     .text('Line chart van geselecteerde landnaam')
 }
+
 // TODO: verander de variabele namen naar kortere namen
-
-
-function update() {
-  lineVaccTypes = [];
+function updateVaccTypes() {
+  vaccTypes = [];
   d3.selectAll('.vacc-type-checkbox').each(function() {
-    if (this.checked) lineVaccTypes.push(this.value);
+    if (this.checked) vaccTypes.push(this.value);
   });
 }
 
-async function main() {
-
-  let lineVaccTypes = ['DTP', 'Pneu', 'Hib', 'Hepb'];
-
-  const data = {
-    'DTP' : [
-      {year:2014, rate:93},
-      {year:2015, rate:91},
-    ],
-    'Pneu' : [
-      {year:1997, rate:3},
-      {year:1998, rate:6},
-    ],
-    'Hib' : [
-      {year:2007, rate:20},
-      {year:2008, rate:28},
-    ],
-    'Hepb' : [
-      {year:2000, rate:60},
-      {year:2001, rate:61},
-    ],
-  };
-
-  xLabels(g_line, graph)
-
-  yLabels(g_line, graph)
-  drawline(g_line, graph, data)
-
-  makeTitles(svg_line, graph)
-
+function update() {
+  updateVaccTypes();
+  drawLines(g_line, graph, lineData[country])
 }
 
-// SVG for line graph
+async function main(data) {
+  lineData = data;
+
+  xLabel(g_line, graph)
+
+  yLabel(g_line, graph)
+
+  makeTitle(svg_line, graph)
+
+  update();
+}
+
 const svg_line = d3.select('.line')
   .attr('width', graph.width + graph.margin.left + graph.margin.right)
   .attr('height', graph.height + graph.margin.top + graph.margin.bottom + graph.margin.padding)
@@ -140,6 +110,19 @@ const g_line = svg_line.append('g')
   .attr('transform', 'translate(' + graph.margin.left + ','
     + (graph.margin.top + graph.margin.padding) + ')');
 
+let country = 'AL';
+let lineData = null;
 
-main()
+const colors = {
+  'DTP': '#a6cee3',
+  'Hib': '#1f78b4',
+  'Hepb': '#b2df8a',
+  'Pneu': '#33a02c',
+};
+
+let vaccTypes = [];
+d3.selectAll(".vacc-type-checkbox").on("change", update);
+
+loadLineData().then(main);
+
 })();

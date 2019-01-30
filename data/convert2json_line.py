1ed03bebcab94974045ea3deabae2313477e27e0
Amber <ambernobel93@gmail.com>
2019-01-30 15:23:23 +0100

    improoved code and added commments

--- data/convert2json_line.py
+++ data/convert2json_line.py
@@ -2,67 +2,66 @@
 # Student number: 11819359
 from collections import defaultdict
 import json
-
 import csv
 
 codes = {
-    "Albania": "AL",
-    "Andorra": "AD",
-    "Austria": "AT",
-    "Azerbaijan": "AZ",
-    "Belarus": "BY",
-    "Belgium": "BE",
-    "Bosnia and Herzegovina": "BA",
-    "Bulgaria": "BG",
-    "Croatia": "HR",
-    "Cyprus": "CY",
-    "Czechia": "CZ",
-    "Denmark": "DK",
-    "Estonia": "EE",
-    "Finland": "FI",
-    "France": "FR",
-    "Georgia": "GE",
-    "Germany": "DE",
-    "Greece": "GR",
-    "Hungary": "HU",
-    "Iceland": "IS",
-    "Ireland": "IE",
-    "Italy": "IT",
-    "Kazakhstan": "KZ",
-    "Kosovo": "XK",
-    "Latvia": "LV",
-    "Liechtenstein": "LI",
-    "Lithuania": "LT",
-    "Luxembourg": "LU",
-    "The former Yugoslav republic of Macedonia": "MK",
-    "Malta": "MT",
-    "Republic of Moldova": "MD",
-    "Monaco": "MC",
-    "Montenegro": "ME",
-    "Netherlands": "NL",
-    "Norway": "NO",
-    "Poland": "PL",
-    "Portugal": "PT",
-    "Romania": "RO",
-    "Russia": "RU",
-    "San Marino": "SM",
-    "Serbia": "RS",
-    "Slovakia": "SK",
-    "Slovenia": "SI",
-    "Spain": "ES",
-    "Sweden": "SE",
-    "Switzerland": "CH",
-    "Turkey": "TR",
-    "Ukraine": "UA",
-    "United Kingdom of Great Britain and Northern Ireland": "GB",
-    "Vatican City": "VA"
+    'Albania': 'AL',
+    'Andorra': 'AD',
+    'Austria': 'AT',
+    'Azerbaijan': 'AZ',
+    'Belarus': 'BY',
+    'Belgium': 'BE',
+    'Bosnia and Herzegovina': 'BA',
+    'Bulgaria': 'BG',
+    'Croatia': 'HR',
+    'Cyprus': 'CY',
+    'Czechia': 'CZ',
+    'Denmark': 'DK',
+    'Estonia': 'EE',
+    'Finland': 'FI',
+    'France': 'FR',
+    'Georgia': 'GE',
+    'Germany': 'DE',
+    'Greece': 'GR',
+    'Hungary': 'HU',
+    'Iceland': 'IS',
+    'Ireland': 'IE',
+    'Italy': 'IT',
+    'Kazakhstan': 'KZ',
+    'Kosovo': 'XK',
+    'Latvia': 'LV',
+    'Liechtenstein': 'LI',
+    'Lithuania': 'LT',
+    'Luxembourg': 'LU',
+    'The former Yugoslav republic of Macedonia': 'MK',
+    'Malta': 'MT',
+    'Republic of Moldova': 'MD',
+    'Monaco': 'MC',
+    'Montenegro': 'ME',
+    'Netherlands': 'NL',
+    'Norway': 'NO',
+    'Poland': 'PL',
+    'Portugal': 'PT',
+    'Romania': 'RO',
+    'Russia': 'RU',
+    'San Marino': 'SM',
+    'Serbia': 'RS',
+    'Slovakia': 'SK',
+    'Slovenia': 'SI',
+    'Spain': 'ES',
+    'Sweden': 'SE',
+    'Switzerland': 'CH',
+    'Turkey': 'TR',
+    'Ukraine': 'UA',
+    'United Kingdom of Great Britain and Northern Ireland': 'GB',
+    'Vatican City': 'VA'
 }
 
 vacc_types = [
-    "Hib",
-    "Pneu",
-    "DTP",
-    "Hepb",
+    'Hib',
+    'Pneu',
+    'DTP',
+    'Hepb',
 ]
 
 years = list(map(str, range(1989, 2018)))
@@ -81,6 +80,7 @@ for vacc_type in vacc_types:
         for row in reader:
             country = row['Country']
 
+            #  skips countries that are not in Europe
             if country not in codes:
                 continue
 
@@ -91,7 +91,8 @@ for vacc_type in vacc_types:
                 for year in years
                 if row.get(year) not in ['', None]
             ]
+            # creates dict like json with land code and vaccin type as keys
             data[code][vacc_type] = values
 
-with open("vacc_line.json", "w") as jsonfile:
+with open('vacc_line.json', 'w') as jsonfile:
     json.dump(data, jsonfile)
